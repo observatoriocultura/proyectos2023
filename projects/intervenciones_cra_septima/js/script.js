8b002425e762f1dabc8865c7f27ce99aac39d058
jmojedap <jmojedap@gmail.com>
2023-07-08 13:19:26 -0500

    Modal with more info

--- projects/intervenciones_cra_septima/js/script.js
+++ projects/intervenciones_cra_septima/js/script.js
@@ -3,15 +3,17 @@
         var intervencionesApp = createApp({
             data(){
                 return{
-                    title: 'HOLA MUNDO',
                     intervenciones: intervenciones,
+                    currentIntervencion: intervenciones[0],
                 }
             },
             methods: {
-                
+                setCurrent: function(key){
+                    this.currentIntervencion = this.intervenciones[key]
+                },
             },
             mounted(){
-                //this.getList()
+                //this.currentIntervencion = 
             }
         }).mount('#intervencionesApp')
     
@@ -30,7 +32,7 @@
             prevArrow: '<div class="slick-prev"> <div class="btn mr-3 btn-warning d-flex justify-content-center align-items-center"> <div>Anterior</div><svg class="ml-1" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve"> <path d="M10.1,19.1l1.5-1.5L7,13h14.1v-2H7l4.6-4.6l-1.5-1.5L3,12L10.1,19.1z"/> </svg></div></div>',
             nextArrow: '<div class="slick-next"> <div class="btn btn-warning d-flex justify-content-center align-items-center"> <svg class="mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M 14 4.9296875 L 12.5 6.4296875 L 17.070312 11 L 3 11 L 3 13 L 17.070312 13 L 12.5 17.570312 L 14 19.070312 L 21.070312 12 L 14 4.9296875 z"/> </svg> <div>Siguiente</div></div></div>',
             dots: true,
-            autoplay: false,
+            autoplay: true,
             speed: 1100,
             slidesToShow: 3,
             slidesToScroll: 3,
